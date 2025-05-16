23caed44a5b5d833a5a2cb44390b8f6a05b5f146
its-shivam1008 <tanil5960@gmail.com>
2025-05-16 17:21:33 +0530

    add dropdown for edit delete and copying the id of the employee in the table row

--- test-proj/src/App.jsx
+++ test-proj/src/App.jsx
@@ -4,6 +4,17 @@ import { BiLoaderAlt } from "react-icons/bi";
 import { Button } from './components/ui/button';
 import { DataTable } from './components/DataTable';
 
+import { MoreHorizontal } from "lucide-react"
+ 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 const App = () => {
 
   const SERVER_URL = 'http://localhost:8080/user'
@@ -198,10 +209,42 @@ const App = () => {
       accessorKey: "country",
       header: "Country",
     },
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        const emp = row.original
+   
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => {
+                  navigator.clipboard.writeText(emp.id)
+                  alert(emp.id)
+                }}
+              >
+                Copy payment ID
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>View customer</DropdownMenuItem>
+              <DropdownMenuItem>View payment details</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )
+      },
+    },
   ]
 
   const dataOk = [ 
     {
+      id:"lol1234",
       name: "Shivam Shukla",
       age: 21,
       dob: "4-12-2003",
@@ -224,7 +267,6 @@ const App = () => {
             
             <form action={handelSubmit}>
               <div className='flex justify-between'>
-              <Button variant="destructive">Button</Button>
               <button type="submit" >Add</button>
               <button type="button" onClick={handleEditUser}>Edit</button>
               <button type="button" onClick={handleFindUser}>Find</button>
