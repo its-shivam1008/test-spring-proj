bcbcce7f8649fc1839a67b6414f0682396dec71f
its-shivam1008 <tanil5960@gmail.com>
2025-05-24 13:29:58 +0530

    form validations

--- test-proj/src/components/AddForm.jsx
+++ test-proj/src/components/AddForm.jsx
@@ -5,101 +5,74 @@ import { toast } from "sonner"
 const AddForm = () => {
 
     const [changeValue, setChangeValue] = useState({name:"", age:"", doj:"", dob:"", mobile:"", address:"", city:"", state:"", country:"", designation:"", salary:""});
+    const [errorSalary, setErrorSalary] = useState(null);
+    const [errorMobile, setErrorMobile] = useState(null);
 
     const [isLoadingPlace, setIsLoadingPlace] = useState(false);
-    const [isLock, setIsLock] = useState(false)
+
+    // useEffect(() => {
+    //   console.log(changeValue);
+    // }, [])
+
+
+    const [isLock, setIsLock] = useState(false);
 
     const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
-    const handleFindUser = async () => {
-        try{
-        const response = await fetch(SERVER_URL+`/${changeValue.name}/${changeValue.dob}`);
-        const result = await response.json();
-        console.log(result);
-        setChangeValue(() =>  ({
-            ...result
-        }));
-        setIsLock(true)
-        }catch(err){
-        alert("some error occured while fetching this employee user"+JSON.stringify(err));
-        }
-    }
-
-    const handleSaveUser = async () => {
-        try{
-          const response = await fetch(SERVER_URL+`/${changeValue.name}/${changeValue.dob}`, {
-            method:"PUT",
-            headers:{"Content-type":"application/json"},
-            body:JSON.stringify(changeValue)
-          });
-          await fetchAllUser();
-          setChangeValue({name:"", age:"", doj:"", dob:"", mobile:"", address:"", city:"", state:"", country:""})
-        }catch(err){
-          alert("some error occured while UPDATING user"+JSON.stringify(err));
-        }
-      }
-
-    const  handleEditUser = () => {
-    setIsLock(false);
-    }
-
     const handleChannge = (event) => {
         const {name, value} = event.target
-        if(changeValue.city == "Kanpur" || changeValue.city == "Agra"){
+        setChangeValue((prev) => {
+          let stateOfCity = "";
+          let ageOfEmployee = "";
+          let salaryOfEmployee = "";
+          let mobileOfEmployee = "";
+          if(name=="salary"){
+            if(/^\d*$/.test(value) && value.length <= 7){
+              salaryOfEmployee = value
+              setErrorSalary(null);
+            }
+            else{
+              setErrorSalary("Field can only contains numbers and be up to 7 digits")
+            }
+          }
           
-          setChangeValue((prev) => (
-            {
+          if(name=="mobile"){
+            if(/^\d*$/.test(value) && value.length <= 10){
+              mobileOfEmployee = value
+              setErrorMobile(null);
+            }
+            else{
+              setErrorMobile("Field can only contains numbers and be up to 10 digits")
+            }
+          }
+
+          if(name == "dob"){
+            ageOfEmployee = (new Date().getFullYear() - new Date(value).getFullYear());
+          }
+
+          if(name == "city" && (value=="Kanpur" || value=="Agra")){
+            stateOfCity = "Uttar Pradesh"
+          }else if(name == "city" && value == "Surat"){
+            stateOfCity = "Gujarat"
+          }else if(name == "city" && value == "Jhansi"){
+            stateOfCity = "Madhaya Pradesh"
+          }
+            return {
               ...prev,
               [name]:value,
               country:"India",
-              age:(new Date().getFullYear() - new Date(changeValue.dob).getFullYear()),
-              state:"Uttar Pradesh"
+              ...(salaryOfEmployee && {salary:salaryOfEmployee}),
+              ...(mobileOfEmployee && {mobile:mobileOfEmployee}),
+              ...(ageOfEmployee && {age:ageOfEmployee}),
+              ...(stateOfCity && {state:stateOfCity})
             }
-          ))
-          setIsLoadingPlace(false)  
-          // console.log(changeValue)
-        }else
-        if(changeValue.city == "Surat"){
-          // setIsLoadingPlace(true)
-          setChangeValue((prev) => (
-            {
-              ...prev,
-              [name]:value,
-              country:"India",
-              age:(new Date().getFullYear() - new Date(changeValue.dob).getFullYear()),
-              state:"Gujarat"
-            }
-          ))
-          setIsLoadingPlace(false)
-        }else
-        if(changeValue.city == "Jhansi"){
-          // setIsLoadingPlace(true);
-          setChangeValue((prev) => (
-            {
-              ...prev,
-              [name]:value,
-              country:"India",
-              age:(new Date().getFullYear() - new Date(changeValue.dob).getFullYear()),
-              state:"Madhya Pradesh"
-            }
-          ))
-          setIsLoadingPlace(false)
-        }else{
-          setChangeValue((prev) => (
-            {
-              ...prev,
-              [name]:value,
-              country:"India",
-              age:(new Date().getFullYear() - new Date(changeValue.dob).getFullYear()),
-            }
-          ))
-    
-        }
-        
+          
+        })
       }
     
       const handelSubmit = async (event) => {
         event.preventDefault;
+        console.log(changeValue);
         try{
           const response =await fetch(SERVER_URL, {
             method:"POST",
@@ -130,8 +103,11 @@ const AddForm = () => {
                   <input type="text" name="designation" id="designation" className='w-50 p-2 rounded-[6px] outline-gray-400 outline-[2px] focus:outline-black placeholder:text-gray-500'  disabled={isLock} onChange={handleChannge} value={changeValue.designation} placeholder='CTO'/>
                 </div>
                 <div className="space-x-4">
-                  <label className={`${isLock ? "text-gray-400" : "text-black"}`} htmlFor="name">Salary </label>
-                  <input type="text" name="salary" id="salary" className='w-50 p-2 rounded-[6px] outline-gray-400 outline-[2px] focus:outline-black placeholder:text-gray-500'  disabled={isLock} onChange={handleChannge} value={changeValue.salary} placeholder='205020'/>
+                  <label className={`${isLock ? "text-gray-400" : "text-black"}`} htmlFor="salary">Salary </label>
+                  <input type="text" name="salary" id="salary" className='w-50 p-2 rounded-[6px] outline-gray-400 outline-[2px] focus:outline-black placeholder:text-gray-500' maxLength={7} disabled={isLock} onChange={handleChannge} value={changeValue.salary} placeholder='205020'/>
+                  {
+                    errorSalary && <div className="text-red-500 text-md font-bold">{errorSalary}</div>
+                  }
                 </div>
                 <div className="space-x-4">
                   <label className={`${isLock ? "text-gray-400" : "text-black"}`} htmlFor="dob">Date of birth </label>
@@ -148,6 +124,9 @@ const AddForm = () => {
                   <div className="space-x-4">
                     <label className={`${isLock ? "text-gray-400" : "text-black"}`} htmlFor="mobile">Mobile </label>
                     <input type="text" name="mobile" id="mobile" className='w-50 p-2 rounded-[6px] outline-gray-400 outline-[2px] focus:outline-black placeholder:text-gray-500' maxLength={10} disabled={isLock} onChange={handleChannge} value={changeValue.mobile}/>
+                    {
+                      errorMobile && <div className="text-red-500 text-md font-bold">{errorMobile}</div>
+                    }
                   </div>
                 <div className="space-x-4">
                   <label className={`${isLock ? "text-gray-400" : "text-black"}`} htmlFor="address">Address </label>
@@ -157,29 +136,21 @@ const AddForm = () => {
                   <label className={`${isLock ? "text-gray-400" : "text-black"}`} htmlFor="city">City </label>
                   <select onChange={handleChannge} value={changeValue.city} name="city" id="city" disabled={isLock} className='w-fit p-2  text-gray-600 text-center rounded-[6px] outline-gray-400 outline-[2px] focus:outline-black placeholder:text-gray-500'>
                     <option value="">Select a city</option>
-                    <option onChange={handleChannge} onClick={() => setIsLoadingPlace(true)} value="Kanpur">Kanpur</option>
-                    <option onChange={handleChannge} onClick={() => setIsLoadingPlace(true)} value="Agra">Agra</option>
-                    <option onChange={handleChannge} onClick={() => setIsLoadingPlace(true)} value="Surat">Surat</option>
-                    <option onChange={handleChannge} onClick={() => setIsLoadingPlace(true)} value="Jhansi">Jhansi</option>
+                    <option onChange={handleChannge} value="Kanpur">Kanpur</option>
+                    <option onChange={handleChannge} value="Agra">Agra</option>
+                    <option onChange={handleChannge} value="Surat">Surat</option>
+                    <option onChange={handleChannge} value="Jhansi">Jhansi</option>
                   </select>
-                  {/* <input type="text" name="city" id="city" className='w-50 p-2 rounded-[6px] outline-gray-400 outline-[2px] focus:outline-black placeholder:text-gray-500' disabled={isLock} onChange={handleChannge} value={changeValue.city}/> */}
                 </div>
                   <div className="space-x-4">
                     <label className={`${isLock ? "text-gray-400" : "text-black"}`} htmlFor="state">State </label>
                     {isLoadingPlace ? <BiLoaderAlt /> : <input type="text" name="state" id="state" className='w-50 p-2 rounded-[6px] outline-gray-400 outline-[2px] focus:outline-amber-500 placeholder:text-gray-500' disabled={isLock} onChange={handleChannge} value={changeValue.state}/>}
                   </div>
                   <div className="space-x-4">
-                    <label className={`${isLock ? "text-gray-400" : "text-black"}`} htmlFor="state">Country </label>
-                    <input type="text" name="state" id="state" className='w-50 p-2 rounded-[6px] outline-gray-400 outline-[2px] focus:outline-amber-500 placeholder:text-gray-500' value={"India"}/>
+                    <label className={`${isLock ? "text-gray-400" : "text-black"}`} htmlFor="country">Country </label>
+                    <input type="text" name="country" id="country" className='w-50 p-2 rounded-[6px] outline-gray-400 outline-[2px] focus:outline-amber-500 placeholder:text-gray-500' value={"India"}/>
                   </div>
                 <Button className='hover:bg-gray-100/50 cursor-pointer transition-colors duration-200 hover:border-black border shadow-lg shadow-orange-200 border-transparent hover:text-black' type="submit" >Add Employee</Button>
-              
-              <div className='hidden'>
-                {/* <Button className='hover:bg-gray-100/50 cursor-pointer transition-colors duration-200 hover:border-black border shadow-lg shadow-orange-200 border-transparent hover:text-black' type="button" variant="secondary" onClick={handleEditUser}>Edit</Button>
-                <Button className='hover:bg-gray-100/50 cursor-pointer transition-colors duration-200 hover:border-black border shadow-lg shadow-orange-200 border-transparent hover:text-black' type="button" variant="secondary" onClick={handleFindUser}>Find</Button>
-                <Button className='hover:bg-gray-100/50 cursor-pointer transition-colors duration-200 hover:border-red-500 border shadow-lg shadow-orange-200 border-transparent hover:text-red-500' variant={'destructive'} type="button" onClick={handleDeleteUser}>Delete</Button>
-                <Button className='hover:bg-gray-100/50 cursor-pointer transition-colors duration-200 hover:border-black border shadow-lg shadow-orange-200 border-transparent hover:text-black' type="button" onClick={handleSaveUser}>Save</Button> */}
-              </div>  
             </form>
     </div>
   )
